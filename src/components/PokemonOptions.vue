00e0cf502a47d373a44768685c291f5c226018c8
lvillen <l.villenjaldon@gmail.com>
2021-09-08 18:04:57 +0200

    who-is-that-pokemon-app  v1.0

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="options-container">
     <ul>
-      <li class="pokemon-name" v-for="pokemon in pokemons" :key="pokemon.id">
+      <li
+        class="pokemon-name"
+        v-for="pokemon in pokemons"
+        :key="pokemon.id"
+        @click="$emit( 'selection', pokemon.id )">
         {{ pokemon.name }}
       </li>
     </ul>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -6,9 +6,21 @@
   <div v-else>
     <h1>Who's that pokémon?</h1>  
 
-    <PokemonImage :pokemonId="pokemon.id" :showPokemon="showPokemon" /> <!-- : == v-bind -->
-    <PokemonOptions :pokemons="pokemonArr" />
+    <PokemonImage
+      :pokemonId="pokemon.id"
+      :showPokemon="showPokemon" /> <!-- : == v-bind -->
+    <PokemonOptions
+      :pokemons="pokemonArr"
+      @selection="checkAnswer" />  
   </div>
+
+  <template v-if="showAnswer">
+    <h2 class="fade-in answer">{{ message }}</h2>
+    <button @click="newGame">
+      New game
+    </button>
+  </template>
+  
 </template>
 
 <script>
@@ -26,7 +38,9 @@ export default {
     return {
       pokemonArr: [],
       pokemon: null,
-      showPokemon: false
+      showPokemon: false,
+      showAnswer: false,
+      message: ''
     }
   },
   methods: {
@@ -35,6 +49,23 @@ export default {
 
       const randomInt = Math.floor( Math.random() * 4 )
       this.pokemon = this.pokemonArr[ randomInt ]
+    },
+    checkAnswer( selectedId ) {
+      this.showPokemon = true
+      this.showAnswer = true
+
+      if ( selectedId === this.pokemon.id ) {
+        this.message = `Great answer, it's ${this.pokemon.name}`
+      } else {
+        this.message = `Oops, wrong answer. It is ${this.pokemon.name}`
+      }
+    },
+    newGame() {
+      this.showPokemon = false
+      this.showAnswer = false
+      this.pokemonArr = []
+      this.pokemon = null
+      this.mixPokemonArray()
     }
   },
   mounted() {
@@ -42,3 +73,22 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.answer {
+  margin-bottom: 40px;
+}
+
+button {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  margin-bottom: 10px;
+  margin-left: 40px;
+}
+</style>
