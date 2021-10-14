b25ff9dad1a388f38c2c19bdd80d7e287b7fd9e1
Luis Villén <l.villenjaldon@gmail.com>
2021-10-14 13:45:35 +0200

    Prune files for deployment

--- tests/unit/api/pokemonApi.spec.js
+++
@@ -1,8 +0,0 @@
-import pokemonApi from '@/api/pokemonApi'
-
-describe('pokemonApi', () => {
-  test('Axios call should use the correct URL', () => {
-
-    expect( pokemonApi.defaults.baseURL ).toBe('https://pokeapi.co/api/v2/pokemon')  
-  })
-})
